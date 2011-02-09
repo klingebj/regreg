fixed divergence problem: was factor of n in the lasso objective

code/regreg/regression.py
@@ -21,6 +21,7 @@ class Regression(object):
 
 class ISTA(Regression):
 
+    debug = False
     def fit(self,L,tol=1e-4,max_its=100,min_its=5):
 
         itercount = 0
@@ -34,10 +35,12 @@ class ISTA(Regression):
                     break
             obj_old = obj
             itercount += 1
-        print "ISTA used", itercount, "iterations"
+        if self.debug:
+            print "ISTA used", itercount, "iterations"
 
 class FISTA(Regression):
 
+    debug = False
     # XXX move L to self.problem...
     def fit(self,L,max_its=100,tol=1e-5,miniter=5):
 
@@ -63,7 +66,8 @@ class FISTA(Regression):
             t_old = t_new
             itercount += 1
 
-        print "FISTA used", itercount, "iterations"
+        if self.debug:
+            print "FISTA used", itercount, "iterations"
     
 class NesterovSmooth(Regression):
     
@@ -78,6 +82,7 @@ class NesterovSmooth(Regression):
 import subfunctions as sf
 class CWPath(Regression):
 
+    debug = False
     def __init__(self, problem, **kwargs):
         self.problem = problem
             
@@ -92,7 +97,7 @@ class CWPath(Regression):
             self.problem.update_cwpath(active,nonzero,1,update_nonzero=True)
             if itercount > min_its:
                 stop, worst = self.stop(bold,tol=tol,return_worst=True)
-                if np.mod(itercount,40)==0:
+                if np.mod(itercount,40)==0 and self.debug:
                     print "Fit iteration", itercount, "with max. relative change", worst
             self.problem.update_cwpath(np.unique(nonzero),nonzero,inner_its)
             itercount += 1

code/tests/lasso_example.py
@@ -9,6 +9,26 @@ control = {'max_its':1500,
            'tol':1.0e-10,
            'plot':False}
 
+def test_FISTA(X=None,Y=None,l1=5., **control):
+
+    if X or Y is None:
+        X = np.load('X.npy')
+        Y = np.load('Y.npy')
+
+    XtX = np.dot(X.T, X)
+    M = np.linalg.eigvalsh(XtX).max() #/ (1*len(Y))
+
+    p3 = lasso.gengrad((X, Y))
+    p3.assign_penalty(l1=l1*X.shape[0])
+
+    t1 = time.time()
+    opt3 = regreg.FISTA(p3)
+    opt3.fit(M,tol=control['tol'], max_its=control['max_its'])
+    beta3 = opt3.problem.coefs
+    t2 = time.time()
+    ts3 = t2-t1
+
+
 def test_lasso(X=None,Y=None,l1=5., **control):
 
     if X or Y is None:
@@ -16,20 +36,20 @@ def test_lasso(X=None,Y=None,l1=5., **control):
         Y = np.load('Y.npy')
 
     XtX = np.dot(X.T, X)
-    M = np.linalg.eigvalsh(XtX).max() / (1*len(Y))
+    M = np.linalg.eigvalsh(XtX).max() #/ (1*len(Y))
 
     Y += np.dot(X[:,:5], 10 * np.ones(5))
     p1 = lasso.cwpath((X, Y))
-    p1.assign_penalty(l1=l1)
+    p1.assign_penalty(l1=l1*X.shape[0])
     
     p2 = lasso.gengrad((X, Y))
-    p2.assign_penalty(l1=l1)
+    p2.assign_penalty(l1=l1*X.shape[0])
 
     p3 = lasso.gengrad((X, Y))
-    p3.assign_penalty(l1=l1)
+    p3.assign_penalty(l1=l1*X.shape[0])
 
     p4 = lasso.gengrad_smooth((X, Y))
-    p4.assign_penalty(l1=l1)
+    p4.assign_penalty(l1=l1*X.shape[0])
 
     t1 = time.time()
     opt1 = regreg.CWPath(p1)
@@ -62,7 +82,6 @@ def test_lasso(X=None,Y=None,l1=5., **control):
     t2 = time.time()
     ts4 = t2-t1
 
-    stop
     assert (np.fabs(beta1-beta3).sum() / np.fabs(beta1).sum() <= 1.0e-04)
     print "Times", ts1, ts2, ts3, ts4
 