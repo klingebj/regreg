fbc2ed6bf5c09ba475bf21f646077b45f4941d4a
klingebj <bjk@stanford.edu>
2011-02-17 11:59:47 -0800

    Group lasso working, but not passing tests due to tolerance of subproblem

--- code/tests/group_lasso.py
+++ code/tests/group_lasso.py
@@ -56,9 +56,9 @@ def test_group_lasso_approximator1(l1=0.1,**control):
     beta3, _ = opt3.output
     X = np.arange(n)
 
-
-    nose.tools.assert_true((np.fabs(beta1-beta2).sum() / np.fabs(beta1).sum()) < 1.0e-04)
     nose.tools.assert_true((np.fabs(beta1-beta3).sum() / np.fabs(beta1).sum()) < 1.0e-04)
+    nose.tools.assert_true((np.fabs(beta1-beta2).sum() / np.fabs(beta1).sum()) < 1.0e-04)
+
 
 def test_group_lasso_approximator2(l1=0.1,**control):
 
@@ -79,21 +79,21 @@ def test_group_lasso_approximator2(l1=0.1,**control):
     D = (np.identity(n) - np.diag(np.ones(n-1),-1))[1:]
     M = np.linalg.eigvalsh(np.dot(D.T, D)).max() 
     
-    p1 = group.group_approximator((Dv, Y))
+    p1 = group.group_approximator((Dv, Y),L=M)
 
-    p3 = signal_approximator.signal_approximator((D, Y))
+    p3 = signal_approximator.signal_approximator((D, Y),L=M)
     p3.assign_penalty(l1=l1*n)
 
     t1 = time.time()
     opt1 = regreg.FISTA(p1)
     opt1.debug = True
-    opt1.fit(M,tol=control['tol'], max_its=control['max_its'])
+    opt1.fit(tol=control['tol'], max_its=control['max_its'])
     t2 = time.time()
     ts1 = t2-t1
 
     t1 = time.time()
     opt3 = regreg.FISTA(p3)
-    opt3.fit(M,tol=control['tol'], max_its=control['max_its'])
+    opt3.fit(tol=control['tol'], max_its=control['max_its'])
     t2 = time.time()
     ts3 = t2-t1
 
@@ -122,29 +122,29 @@ def test_group_lasso(l1=0.1,**control):
     D = (np.identity(n) - np.diag(np.ones(n-1),-1))[1:]
     M = np.linalg.eigvalsh(np.dot(D.T, D)).max() 
     
-    p1 = group.group_approximator((Dv, Y))
+    p1 = group.group_approximator((Dv, Y),L=M)
 
-    p2 = group.group_lasso((np.identity(n), Dv, Y))
+    p2 = group.group_lasso((np.identity(n), Dv, Y),L=M)
 
-    p3 = signal_approximator.signal_approximator((D, Y))
+    p3 = signal_approximator.signal_approximator((D, Y),L=M)
     p3.assign_penalty(l1=l1*n)
 
     t1 = time.time()
     opt1 = regreg.FISTA(p1)
     opt1.debug = True
-    opt1.fit(M,tol=control['tol'], max_its=control['max_its'])
+    opt1.fit(tol=control['tol'], max_its=control['max_its'])
     t2 = time.time()
     ts1 = t2-t1
 
     t1 = time.time()
     opt2 = regreg.FISTA(p3)
-    opt2.fit(M,tol=control['tol'], max_its=control['max_its'])
+    opt2.fit(tol=control['tol'], max_its=control['max_its'])
     t2 = time.time()
     ts3 = t2-t1
 
     t1 = time.time()
     opt3 = regreg.FISTA(p3)
-    opt3.fit(M,tol=control['tol'], max_its=control['max_its'])
+    opt3.fit(tol=control['tol'], max_its=control['max_its'])
     t2 = time.time()
     ts3 = t2-t1
 
@@ -173,7 +173,7 @@ def test_group_lasso2(l1=1,**control):
         z[i] = 1.
         return z
 
-    p1 = lasso.gengrad((X, Y))
+    p1 = lasso.gengrad((X, Y),L=M)
     p1.assign_penalty(l1=l1*n)
 
     Dv = [(e(i, p), l1*n) for i in range(p)]
@@ -182,23 +182,23 @@ def test_group_lasso2(l1=1,**control):
 
     t1 = time.time()
     opt1 = regreg.FISTA(p1)
-    opt1.fit(M,tol=control['tol'], max_its=control['max_its'])
+    opt1.fit(tol=control['tol'], max_its=control['max_its'])
     t2 = time.time()
     ts1 = t2-t1
 
     t1 = time.time()
     opt2 = regreg.FISTA(p2)
     opt2.debug = True
-    opt2.fit(M,tol=control['tol'], max_its=control['max_its'])
+    opt2.fit(tol=control['tol'], max_its=control['max_its'])
     t2 = time.time()
     ts3 = t2-t1
 
-    p3 = glasso.generalized_lasso((X, np.identity(p), Y))
+    p3 = glasso.generalized_lasso((X, np.identity(p), Y),L=M)
     p3.assign_penalty(l1=l1*n)
     p3.dualcontrol['tol'] = 1.0e-10
     t1 = time.time()
     opt3 = regreg.FISTA(p3)
-    opt3.fit(M,tol=control['tol'], max_its=control['max_its'])
+    opt3.fit(tol=control['tol'], max_its=control['max_its'])
     t2 = time.time()
     ts3 = t2-t1
 
@@ -229,24 +229,24 @@ def test_group_lasso3(l1=3,**control):
         return z
 
     Dv = [(e(i, p), l1*n) for i in range(p-1)]
-    p2 = group.group_lasso((X, Dv, Y))
+    p2 = group.group_lasso((X, Dv, Y),L=M)
     p2.dualcontrol['tol'] = 1.0e-10
 
     D = (np.identity(p) - np.diag(np.ones(p-1),-1))[1:]
-    p3 = glasso.generalized_lasso((X, D, Y))
+    p3 = glasso.generalized_lasso((X, D, Y),L=M)
     p3.dualcontrol['tol'] = 1.0e-10
     p3.assign_penalty(l1=l1*n)
 
     t1 = time.time()
     opt2 = regreg.FISTA(p2)
     opt2.debug = True
-    opt2.fit(M,tol=control['tol'], max_its=control['max_its'])
+    opt2.fit(tol=control['tol'], max_its=control['max_its'])
     t2 = time.time()
     ts3 = t2-t1
 
     t1 = time.time()
     opt3 = regreg.FISTA(p3)
-    opt3.fit(M,tol=control['tol'], max_its=control['max_its'])
+    opt3.fit(tol=control['tol'], max_its=control['max_its'])
     t2 = time.time()
     ts3 = t2-t1
 
