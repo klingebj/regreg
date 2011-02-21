61bb8830006ef72b21e54c8b2455c4adceca2b1f
klingebj <bjk@stanford.edu>
2011-02-21 14:55:18 -0800

    Added lin_graphnet.py for use with multicca

---
+++ code/regreg/lin_graphnet.py
@@ -0,0 +1,171 @@
+import numpy as np
+
+# Local imports
+
+import subfunctions as sf
+import updates
+import l1smooth
+from problems import linquadmodel
+
+class lin_graphnet(linquadmodel):
+
+    """
+    GraphNet problem:
+    Minimizes
+
+    .. math::
+
+       \begin{eqnarray}
+       y^T\beta + \lambda_{1}\|\beta\|_{1} + \lambda_2 \|\beta\|_{2}^{2} + \lambda_3 \beta^T L \beta
+       \end{eqnarray}
+
+    as a function of beta.
+    """
+
+    def initialize(self, data):
+        """
+        Generate initial tuple of arguments for update.
+        """
+
+        if len(data) == 3:
+            self.X = data[0]
+            self.Y = data[1]
+            self.Lap = data[2]
+            self.n, self.p = self.X.shape
+        else:
+            raise ValueError("Data tuple not as expected")
+
+        if hasattr(self,'initial_coefs'):
+            self.set_coefs(self.initial_coefs)
+        else:
+            self.set_coefs(self.default_coefs)
+            
+    @property
+    def default_penalties(self):
+        """
+        Default penalties for GraphNet
+        """
+        return np.zeros(1, np.dtype([(l, np.float) for l in ['l1', 'l2', 'l3']]))
+
+    def obj(self, beta):
+        beta = np.asarray(beta)
+        return self.obj_smooth(beta) + self.obj_rough(beta)
+
+    def obj_smooth(self, beta):
+        #Smooth part of objective
+        return -np.dot(self.Y, beta) + np.dot(beta,beta) * self.penalties['l2'] + np.dot(beta,np.dot(self.Lap,beta)) * self.penalties['l3']
+
+    def obj_rough(self, beta):
+        return np.sum(np.fabs(beta)) * self.penalties['l1'] 
+
+
+class gengrad(lin_graphnet):
+    
+    def grad(self, beta):
+        beta = np.asarray(beta)
+        return -self.Y + 2*beta*self.penalties['l2'] + 2*np.dot(self.Lap,beta)*self.penalties['l3']
+
+    def proximal(self, z, g, L):
+        v = z - g / L
+        return np.sign(v) * np.maximum(np.fabs(v)-self.penalties['l1']/L, 0)
+
+
+class lin_graphnet_sparse(lin_graphnet):
+    
+    #Assume Lap given as scipy.sparse matrix
+    def obj_smooth(self, beta):
+        beta = np.asarray(beta)
+        return -np.dot(self.Y, beta) + np.dot(beta,beta) * self.penalties['l2'] + np.dot(beta,self.Lap.matvec(beta)) * self.penalties['l3']
+
+class gengrad_sparse(lin_graphnet_sparse):
+    
+    def grad(self, beta):
+        beta = np.asarray(beta)
+        return -self.Y + 2*beta*self.penalties['l2'] + 2*self.Lap.matvec(beta)*self.penalties['l3']
+
+    def proximal(self, z, g, L):
+        v = z - g / L
+        return np.sign(v) * np.maximum(np.fabs(v)-self.penalties['l1']/L, 0)
+
+         
+
+"""
+class gengrad_smooth(gengrad):
+
+    def smooth(self, L, epsilon):
+        return l1smooth.l1smooth(self.grad, L, epsilon, l1=self.penalties['l1'], f=self.obj)
+
+
+class cwpath(lasso):
+
+    def __init__(self, data, penalties={}, initial_coefs=None):
+        lasso.__init__(self, data, penalties=penalties,
+                       initial_coefs=initial_coefs)
+
+    # Differences to the standard initialization
+        
+    def initialize(self, data):
+        lasso.initialize(self, data)
+        self._ssq = sf.col_ssq(self.X)
+
+    # Residuals must be updated if coefs change
+
+    def set_coefs(self, coefs):
+        self._coefs = coefs
+        self.update_residuals()
+
+    def get_coefs(self):
+        if not hasattr(self, "_coefs"):
+            self._coefs = self.default_coefs
+        return self._coefs
+    coefs = property(get_coefs, set_coefs)
+
+    def update_residuals(self):
+        self.r = self.Y - np.dot(self.X,self.coefs)
+
+    # Inner products also must be updated if inner response changes
+    def set_response(self,Y):
+        self._Y = Y
+        self.update_residuals()
+        self.inner = sf.col_inner(self.X,self._Y)
+
+    def get_response(self):
+        return self._Y
+    Y = property(get_response, set_response)
+
+    def update_cwpath(self,
+                      active,
+                      nonzero,
+                      inner_its = 1,
+                      permute = False,
+                      update_nonzero = False):
+   
+      
+        if permute:
+            active = np.random.permutation(active)
+        if len(active):
+            updates._update_lasso_cwpath(active,
+                                         self.penalties,
+                                         nonzero,
+                                         self.coefs,
+                                         self.r,
+                                         self.X,
+                                         self._ssq,
+                                         inner_its,
+                                         update_nonzero)
+
+    def set_rowweights(self, weights):
+        if weights is not None:
+            self.rowwts = weights
+            if self.update_resids:
+                self.update_residuals()
+            if hasattr(self,'_ssq'):
+                self._ssq = sf.col_ssq(self.X,self.rowwts)
+
+    def get_rowweights(self):
+        if hasattr(self,'rowwts'):
+            return self.rowwts.copy()
+        else:
+            return None
+    rowweights = property(get_rowweights, set_rowweights)
+"""
---
+++ code/tests/test_lin_graphnet.py
@@ -0,0 +1,65 @@
+import numpy as np
+import pylab, time, scipy
+import scipy.sparse
+import regreg.regression as regreg
+import regreg.lasso as lasso
+import regreg.graphnet as graphnet
+import regreg.lin_graphnet as lin_graphnet
+import regreg.signal_approximator as glasso
+from tests import gen_adj        
+import nose.tools
+
+control = {'max_its':500,
+           'tol':1.0e-8,
+           'plot':False,
+           'backtrack':True}
+
+def test_lin_graphnet(X=None,Y=None,l1=5.,l2=0.,l3=0., control=control):
+
+    if X is None or Y is None:
+        X = np.load('X.npy')
+        Y = np.load('Y.npy')
+
+
+    p = X.shape[1]
+    _ , L = gen_adj(p)
+    Lsparse = scipy.sparse.lil_matrix(L)
+
+    Y = np.dot(X.T,Y)
+    Y += - np.mean(Y)
+    
+    l1 *= X.shape[0]
+    p1 = lin_graphnet.gengrad((X, Y, L))
+    p1.assign_penalty(l1=l1,l2=l2,l3=l3)
+    t1 = time.time()
+    opt1 = regreg.FISTA(p1)
+    #opt1.debug = True
+    opt1.fit(tol=control['tol'], max_its=control['max_its'])
+    beta1 = opt1.problem.coefs
+    t2 = time.time()
+    ts3 = t2-t1
+
+    p2 = lin_graphnet.gengrad_sparse((X, Y, Lsparse))
+    p2.assign_penalty(l1=l1,l2=l2,l3=l3)
+    t1 = time.time()
+    opt2 = regreg.FISTA(p2)
+    opt2.fit(tol=control['tol'], max_its=control['max_its'])
+    beta2 = opt2.problem.coefs
+    t2 = time.time()
+    ts3 = t2-t1
+
+
+
+    def f(beta):
+        return - np.dot(Y, beta) + np.fabs(beta).sum()*l1 + l2 * np.linalg.norm(beta)**2 + l3 * np.dot(beta, np.dot(L, beta))
+    
+    v = scipy.optimize.fmin_powell(f, np.zeros(len(Y)), ftol=1.0e-10, xtol=1.0e-10,maxfun=100000)
+    v = np.asarray(v)
+
+
+    N = 10000
+    print np.round(N*beta1)/N
+    print np.round(N*beta2)/N
+    print np.round(N*v)/N
+    print f(beta1), f(v)
+
