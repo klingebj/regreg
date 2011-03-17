c75c978cbc9786f23fb221452b22d5056cf6e1c5
Jonathan Taylor <jonathan.taylor@stanford.edu>
2011-03-16 17:29:48 -0700

    grouped is very slow -- weird infinities?

--- code/regreg/seminorm.py
+++ code/regreg/seminorm.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+import regression; reload(regression)
 from regression import FISTA, ISTA
 from problems import linmodel
 
@@ -52,24 +53,14 @@ class squaredloss(problem):
     """
 
 
-    def initialize(self, data):
+    def __init__(self, X, Y):
         """
         Generate initial tuple of arguments for update.
         """
 
-        if len(data) == 2:
-            self.X = data[0]
-            self.Y = data[1]
-            self.n, self.p = self.X.shape
-            self.semi.p = self.p
-        else:
-            raise ValueError("Data tuple not as expected")
-
-        if hasattr(self,'initial_coefs'):
-            self.set_coefs(self.initial_coefs)
-        else:
-            self.set_coefs(self.default_coefs)
-            
+        self.X = X
+        self.Y = Y
+        self.n, self.p = self.X.shape
 
     def obj_smooth(self, beta):
         #Smooth part of objective
@@ -79,21 +70,27 @@ class squaredloss(problem):
         XtXbeta = np.dot(self.X.T, np.dot(self.X, beta)) 
         return XtXbeta - np.dot(self.Y,self.X) 
 
+    def add_seminorm(self, seminorm, initial=None, smooth_multiplier=1):
+        return seminorm.problem(self.obj_smooth, self.grad, smooth_multiplier=smooth_multiplier,
+                                initial=initial)
   
 class dummy_problem(object):
     """
     A generic way to specify a problem
     """
-    def __init__(self, smooth, grad_smooth, nonsmooth, prox, initial, L):
+    def __init__(self, smooth, grad_smooth, nonsmooth, prox, initial, smooth_multiplier=1):
         self.coefs = initial * 1.
         self.obj_smooth = smooth
         self.nonsmooth = nonsmooth
-        self.grad = grad_smooth
+        self._grad = grad_smooth
         self._prox = prox
-        self.L = L
+        self.smooth_multiplier = smooth_multiplier
 
     def obj(self, x):
-        return self.obj_smooth(x) + self.nonsmooth(x)
+        return self.smooth_multiplier * self.obj_smooth(x) + self.nonsmooth(x)
+
+    def grad(self, x):
+        return self.smooth_multiplier * self._grad(x)
 
     def proximal(self, x, g, L):
         z = x - g / L
@@ -105,13 +102,16 @@ class seminorm_atom(object):
     A seminorm atom class
     """
 
+    #XXX spec as 1d array could mean weights?
+    #XXX matrix multiply should be sparse if possible
     def __init__(self, spec, l=1.):
         if type(spec) == type(1):
             self.p = self.m = spec
             self.D = None
         else:
             D = spec
-            D = D.reshape((1,-1))
+            if D.ndim == 1:
+                D = D.reshape((1,-1))
             self.D = D
             self.m, self.p = D.shape
         self.l = l
@@ -161,6 +161,17 @@ class seminorm_atom(object):
         else:
             return x
 
+    def problem(self, smooth, grad_smooth, smooth_multiplier=1., initial=None):
+        """
+        Return a problem instance 
+        """
+        prox = self.primal_prox
+        nonsmooth = self.evaluate
+        if initial is None:
+            initial = np.random.standard_normal(self.p)
+        return dummy_problem(smooth, grad_smooth, nonsmooth, prox, initial, smooth_multiplier)
+
+
 class l1norm(seminorm_atom):
 
     """
@@ -176,7 +187,14 @@ class l1norm(seminorm_atom):
         else:
             return self.l * np.fabs(np.dot(self.D, x)).sum()
 
-    def primal_prox(self, x,  L):
+    def evaluate_dual(self, u):
+        inbox = np.product(np.less_equal(np.fabs(u), self.l))
+        if inbox:
+            return 0
+        else:
+            return np.inf
+
+    def primal_prox(self, x,  L=1):
         """
         Return (unique) minimizer
 
@@ -194,11 +212,11 @@ class l1norm(seminorm_atom):
         """
 
         if self.D is None:
-            return np.sign(v) * np.maximum(np.fabs(v)-self.l/L, 0)
+            return np.sign(x) * np.maximum(np.fabs(x)-self.l/L, 0)
         else:
             return FISTAsoln # will barf
 
-    def dual_prox(self, u,  L):
+    def dual_prox(self, u, L=1):
         """
         Return a minimizer
 
@@ -210,35 +228,32 @@ class l1norm(seminorm_atom):
         where *m*=u.shape[0], :math:`\lambda`=self.l. 
         This is just truncation: np.clip(u, -self.l/L, self.l/L).
         """
-        return np.clip(u, -self.l/L, self.l/L)
+        return np.clip(u, -self.l, self.l)
 
 class l2norm(seminorm_atom):
 
     """
-    The l1 norm
+    The l2 norm
     """
-
-    def __init__(self, spec, l=1.):
-        if type(spec) == type(1):
-            self.p = self.m = spec
-            self.D = None
-        else:
-            D = spec
-            D = D.reshape((1,-1))
-            self.D = D
-            self.m, self.p = D.shape
-        self.l = l
-        
+    tol = 1.0e-10
+    
     def evaluate(self, x):
         """
         The L2 norm of Dx.
         """
         if self.D is not None:
-            return self.l * np.sqrt((np.dot(self.D,x)**2).sum())
+            return self.l * np.linalg.norm(np.dot(self.D, x))
         else:
-            return self.l * np.sqrt((x**2).sum())
+            return self.l * np.linalg.norm(x)
 
-    def primal_prox(self, x,  L):
+    def evaluate_dual(self, u):
+        inball = (np.linalg.norm(u) <= self.l)
+        if inball:
+            return 0
+        else:
+            return np.inf
+
+    def primal_prox(self, x,  L=1):
         """
         Return (unique) minimizer
 
@@ -252,15 +267,19 @@ class l2norm(seminorm_atom):
 
         .. math::
 
-            v^{\lambda}(x) = \max(1 - \frac{\lambda/L}{\|x\|_2}, 0) x
+            v^{\lambda}(x) = \max(1 - \frac{\lambda}{\|x\|_2}, 0) x
         """
 
         if self.D is None:
-            return x - self.dual_prox(x, L)
+            n = np.linalg.norm(x)
+            if n >= self.l / L:
+                return np.zeros(x.shape)
+            else:
+                return (1 - self.l / (L*n) * (1 - l2norm.tol)) * x
         else:
-            return FISTAsoln
+            return FISTAsoln # this will barf
 
-    def dual_prox(self, u,  L):
+    def dual_prox(self, u,  L=1):
         """
         Return a minimizer
 
@@ -276,8 +295,11 @@ class l2norm(seminorm_atom):
 
             v^{\lambda}(u) = \min(1, \frac{\lambda/L}{\|u\|_2}) u
         """
-        n = self.evaluate(u)
-        return min(1, (self.l/L) / n) * u
+        n = np.linalg.norm(u)
+        if n < self.l:
+            return u
+        else:
+            return (self.l * (1 - l2norm.tol)/ n) * u
 
 class seminorm(object):
     """
@@ -313,13 +335,19 @@ class seminorm(object):
             out += atom.evaluate(x)
         return out
     
-    def dual_prox(self, u, L):
+    def evaluate_dual(self, u):
+        out = 0.
+        for atom, segment in zip(self.atoms, self.segments):
+            out += atom.evaluate_dual(u[segment])
+        return out
+    
+    def dual_prox(self, u, L_D=1.):
         """
         Return (unique) minimizer
 
         .. math::
 
-           v^{\lambda}(u) = \text{argmin}_{v \in \real^m} \frac{L}{2}
+           v^{\lambda}(u) = \text{argmin}_{v \in \real^m} \frac{1}{2}
            \|v-u\|^2_2  s.t.  h^*_i(v) \leq \infty, 0 \leq i \leq M-1
 
         where *m*=u.shape[0]=np.sum(self.dual_dims), :math:`M`=self.M
@@ -332,42 +360,136 @@ class seminorm(object):
         """
         v = np.empty(u.shape)
         for atom, segment in zip(self.atoms, self.segments):
-            v[segment] = atom.dual_prox(v[segment], atom.l/L)
+            v[segment] = atom.dual_prox(u[segment], L_D)
         return v
 
-    def primal_prox(self, x, L, solver=ISTA):
-        dualp = self.dual_problem(x, L=L, solver=solver)
-        dualp.debug = True
-        dualp.fit(max_its=20000)
-        return self.primal_from_dual(x, dualp.problem.coefs)
+    default_solver = ISTA
+    def primal_prox(self, y, L_P=1, with_history=False, debug=False):
+        #XXX make dualp persistent...
+        dualp = self.dual_problem(y, L_P=L_P)
+        solver = seminorm.default_solver(dualp)
+        solver.debug = debug
+        history = solver.fit(max_its=2000)
+        print history.shape, L_P, np.max(history), np.min(history), 'niterations'
+        if with_history:
+            return self.primal_from_dual(y, solver.problem.coefs), history
+        else:
+            return self.primal_from_dual(y, solver.problem.coefs)
 
-    def primal_from_dual(self, u, v):
+    def primal_from_dual(self, y, u):
         """
 
         """
-        x = u * 1.
+        x = y * 1.
         for atom, segment in zip(self.atoms, self.segments):
-            x -= atom.multiply_by_DT(v[segment])
+            x -= atom.multiply_by_DT(u[segment])
         return x
 
-    def dual_problem(self, y, L=1, solver=FISTA, initial=None):
+    def dual_problem(self, y, L_P=1, initial=None):
         """
         Return a problem instance of the dual
         prox problem with a given y value.
         """
-        def smooth(v,y=y):
+        
+        def smooth(v):
             primal = self.primal_from_dual(y, v)
-            return L * (primal**2).sum() / 2.
-        def grad_smooth(v,y=y):
+            return (primal**2).sum() / 2.
+        def grad_smooth(v):
             primal = self.primal_from_dual(y, v)
             g = np.zeros(self.total_dual)
             for atom, segment in zip(self.atoms, self.segments):
-                g[segment] = atom.multiply_by_D(primal)
-            g *= -L
+                g[segment] = -atom.multiply_by_D(primal)
             return g
-        prox = self.dual_prox
-        nonsmooth = self.evaluate #XXX this should be the constraint one
         if initial is None:
-            initial = np.random.standard_normal(self.total_dual)
-        return solver(dummy_problem(smooth, grad_smooth, nonsmooth, prox, initial, L))
+            z = np.random.standard_normal(self.total_dual)
+            initial = self.dual_prox(z, 1./L_P)
+        nonsmooth = self.evaluate_dual
+        prox = self.dual_prox
+        return dummy_problem(smooth, grad_smooth, nonsmooth, prox, initial, 1./L_P)
+
+    def problem(self, smooth, grad_smooth, smooth_multiplier=1., initial=None):
+        prox = self.primal_prox
+        nonsmooth = self.evaluate
+        if initial is None:
+            initial = self.dual_prox(np.random.standard_normal(self.primal_dim))
+        return dummy_problem(smooth, grad_smooth, nonsmooth, prox, initial, smooth_multiplier)
+        
+import pylab
+
+def fused_example():
+
+    x=np.random.standard_normal(500); x[100:150] += 7
+
+    sparsity = l1norm(500, l=1.3)
+    D = (np.identity(500) + np.diag([-1]*499,k=1))[:-1]
+    fused = l1norm(D, l=1.5)
+
+    pen = seminorm(sparsity,fused)
+    soln, vals = pen.primal_prox(x, 1)
+    
+    # solution
+
+    pylab.figure(num=1)
+    pylab.clf()
+    pylab.plot(soln, c='g')
+    pylab.scatter(np.arange(x.shape[0]), x)
+
+    # objective values
+
+    pylab.figure(num=2)
+    pylab.clf()
+    pylab.plot(vals)
+
+def lasso_example():
+
+    sparsity = l1norm(500, l=20.)
+    X = np.random.standard_normal((1000,500))
+    Y = np.random.standard_normal((1000,))
+    regloss = squaredloss(X,Y)
+    p=regloss.add_seminorm(sparsity)
+    solver=ISTA(p)
+    solver.debug = True
+    vals = solver.fit(max_its=2000)
+    soln = solver.problem.coefs
+
+    # solution
+
+    pylab.figure(num=1)
+    pylab.clf()
+    pylab.plot(soln, c='g')
+
+    # objective values
+
+    pylab.figure(num=2)
+    pylab.clf()
+    pylab.plot(vals)
+
+def group_lasso_example():
+
+    def selector(p, slice):
+        return np.identity(p)[slice]
+    penalties = [l2norm(selector(500, slice(i*100,(i+1)*100)), l=100.) for i in range(5)]
+    group_lasso = seminorm(*penalties)
+    x = np.random.standard_normal(500)
+    group_lasso.primal_prox(x, 1.)
+    X = np.random.standard_normal((1000,500))
+    Y = np.random.standard_normal((1000,))
+    regloss = squaredloss(X,Y)
+    p=regloss.add_seminorm(group_lasso, smooth_multiplier=1.0e-03)
+    solver=ISTA(p)
+    solver.debug = True
+    vals = solver.fit(max_its=2000, start_inv_step=1000.)
+    soln = solver.problem.coefs
+
+    # solution
+
+    pylab.figure(num=1)
+    pylab.clf()
+    pylab.plot(soln, c='g')
+
+    # objective values
+
+    pylab.figure(num=2)
+    pylab.clf()
+    pylab.plot(vals)
 
