de19785f00cefbd5201005845eddf733dc8c48e6
klingebj <bjk@stanford.edu>
2011-03-23 14:29:11 -0700

    Moving old framework files to subdir

--- code/regreg/old_framework/regression.py
+++ code/regreg/old_framework/regression.py
@@ -1,9 +1,11 @@
 import numpy as np
 
-class Regression(object):
+class algorithm(object):
 
     def __init__(self, problem):
         self.problem = problem
+        self.debug = False
+        self.inv_step = None
 
     @property
     def output(self):
@@ -19,10 +21,8 @@ class Regression(object):
         """
         raise NotImplementedError
 
-class ISTA(Regression):
+class ISTA(algorithm):
 
-    debug = False
-    inv_step = None
     def fit(self,tol=1e-4,max_its=100,min_its=5,backtrack=True,alpha=1.1,start_inv_step=1.):
 
         objective_hist = np.zeros(max_its)
@@ -69,32 +69,41 @@ class ISTA(Regression):
             print "ISTA used", itercount, "iterations"
         return objective_hist
 
-class FISTA(Regression):
+class FISTA(algorithm):
 
-    debug = False
-    inv_step = None
-    def fit(self,max_its=100,tol=1e-5,min_its=5,backtrack=True,alpha=1.1,start_inv_step=1.,restart=np.inf):
+    def fit(self,
+            max_its=100,
+            min_its=5,
+            tol=1e-5,
+            backtrack=True,
+            alpha=1.1,
+            start_inv_step=1.,
+            restart=np.inf,
+            coef_stop=False):
 
         objective_hist = np.zeros(max_its)
-        f = self.problem.obj
-        r = self.problem.coefs
-        t_old = 1.
-        obj_cur = np.inf
         itercount = 0
         if self.inv_step is None:
+            #If available, use Lipschitz constant from last fit
             self.inv_step = start_inv_step
+        r = self.problem.coefs
+        t_old = 1.
         beta = self.problem.coefs
+        current_f = self.problem.obj_smooth(r)
+        current_obj = self.problem.obj(self.problem.coefs)
         while itercount < max_its:
+
+            #Restart every restart iterations
             if np.mod(itercount+1,restart)==0:
                 if self.debug:
                     print "Restarting"
                 r = self.problem.coefs
                 t_old = 1.
                 self.inv_step *= 0.5            
-            f_beta = f(self.problem.coefs)
-            obj_cur = f_beta
-            objective_hist[itercount] = obj_cur
+
+            objective_hist[itercount] = current_obj
             grad = self.problem.grad(r)
+
             # Backtracking loop
             if backtrack:
                 current_f = self.problem.obj_smooth(r)
@@ -110,98 +119,57 @@ class FISTA(Regression):
                         stop = np.fabs(np.dot(beta-r,grad-trial_grad)) <= 0.5*self.inv_step*np.linalg.norm(beta-r)**2
                     if not stop:
                         self.inv_step *= alpha
+                        #if self.debug:
+                        #    print self.inv_step
             else:
+                #Use specified Lipschitz constant
                 self.inv_step = self.problem.L
                 beta = self.problem.proximal(r, grad, self.inv_step)
-
-            trial_obj = f(beta)
+                trial_f = self.problem.obj_smooth(beta)
+                
+            trial_obj = trial_f + self.problem.obj_rough(beta)
+            obj_change = np.fabs(trial_obj - current_obj)
+            obj_rel_change = obj_change/current_obj 
 
             if self.debug:
-                print itercount, obj_cur, self.inv_step, (trial_obj-obj_cur)/obj_cur, np.linalg.norm(self.problem.coefs - beta) / np.max([1.,np.linalg.norm(beta)])
+                print itercount, current_obj, self.inv_step, obj_rel_change, np.linalg.norm(self.problem.coefs - beta) / np.max([1.,np.linalg.norm(beta)]), tol
 
 
             if itercount >= min_its:
-                if np.linalg.norm(self.problem.coefs - beta) / np.max([1.,np.linalg.norm(beta)]) < tol:
-                    self.problem.coefs = beta
-                    break
-                if np.fabs(trial_obj - obj_cur)/obj_cur < tol:
-                    self.problem.coefs = beta
-                    break
+                if not coef_stop:
+                    if obj_rel_change < tol:
+                        self.problem.coefs = beta
+                        break
+                else:
+                    if np.linalg.norm(self.problem.coefs - beta) / np.max([1.,np.linalg.norm(beta)]) < tol:
+                        self.problem.coefs = beta
+                        break
+            else:
+                if not coef_stop:
+                    if obj_change < 0.:
+                        self.problem.coefs = beta
+                        break                
+                    
 
 
             t_new = 0.5 * (1 + np.sqrt(1+4*(t_old**2)))
             r = beta + ((t_old-1)/(t_new)) * (beta - self.problem.coefs)
-            if (trial_obj - obj_cur)/trial_obj > 1e-12:
+            
+            if current_obj < trial_obj and obj_rel_change > 1e-10:
+                #Adaptive restarting: restart if monotonicity violated
                 if self.debug:
-                    print "Restarting", trial_obj, obj_cur
+                    print "Restarting", trial_obj, current_obj
                 t_old = 1.
                 r = self.problem.coefs
             else:
                 self.problem.coefs = beta
                 t_old = t_new
-            itercount += 1
+                itercount += 1
+                current_obj = trial_obj
+            
 
         if self.debug:
             print "FISTA used", itercount, "iterations"
         return objective_hist[:itercount]
-    
-class NesterovSmooth(Regression):
-    
-    def fit(self,tol=1e-4,epsilon=0.1,max_its=100):
-        import nesterov_smooth
-        p = len(self.problem.coefs)
-        grad_s, L_s, f_s = self.problem.smooth(self.problem.L, epsilon)
-        self.problem.coefs, l = nesterov_smooth.loop(self.problem.coefs, grad_s, L_s, f=f_s, maxiter=max_its, values=True, tol=tol)
-        return f_s
 
 
-import subfunctions as sf
-class CWPath(Regression):
-
-    debug = False
-    def __init__(self, problem, **kwargs):
-        self.problem = problem
-            
-    def fit(self,tol=1e-4,inner_its=50,max_its=2000,min_its=5):
-        
-        active = np.arange(self.problem.coefs.shape[0])
-        itercount = 0
-        stop = False
-        while not stop and itercount < max_its:
-            bold = self.output
-            nonzero = []
-            self.problem.update_cwpath(active,nonzero,1,update_nonzero=True)
-            if itercount > min_its:
-                stop, worst = self.stop(bold,tol=tol,return_worst=True)
-                if np.mod(itercount,40)==0 and self.debug:
-                    print "Fit iteration", itercount, "with max. relative change", worst
-            self.problem.update_cwpath(np.unique(nonzero),nonzero,inner_its)
-            itercount += 1
-
-    def stop(self,
-             previous,
-             tol=1e-4,
-             return_worst = False):
-        """
-        Convergence check: check whether 
-        residuals have not significantly changed or
-        they are small enough.
-
-        Both old and current are expected to be (beta, r) tuples, i.e.
-        regression coefficent and residual tuples.
-    
-        """
-
-        bold, _ = previous
-        bcurrent, _ = self.output
-
-        if return_worst:
-            status, worst = sf.coefficientCheckVal(bold, bcurrent, tol)
-            if status:
-                return True, worst
-            return False, worst
-        else:
-            status = sf.coefficientCheck(bold, bcurrent, tol)
-            if status:
-                return True
-            return False
