e3d8ad3b33b33dfa783a3ee98cfc44acfede283e
Jonathan Taylor <jonathan.taylor@stanford.edu>
2011-04-22 01:09:37 -0700

    moved dummy problem to its own tiny module -- don't know where to put it just now

--- code/regreg/seminorm.py
+++ code/regreg/seminorm.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy import sparse
 from algorithms import FISTA, ISTA
+from problem import dummy_problem
 
 class seminorm(object):
     """
@@ -162,42 +163,4 @@ class seminorm(object):
         return dummy_problem(smooth_eval, nonsmooth, prox, initial, smooth_multiplier)
 
 
-
-class dummy_problem(object):
-    """
-    A generic way to specify a problem
-    """
-    def __init__(self, smooth_eval, nonsmooth, prox, initial, smooth_multiplier=1):
-        # Do we need to store this?
-        #self.initial = initial.copy()
-        self.coefs = initial.copy()
-        self.obj_rough = nonsmooth
-        self._smooth_eval = smooth_eval
-        self._prox = prox
-        self.smooth_multiplier = smooth_multiplier
-
-
-    def smooth_eval(self, x, mode='both'):
-        output = self._smooth_eval(x, mode=mode)
-        if mode == 'both':
-            return self.smooth_multiplier * output[0], self.smooth_multiplier * output[1]
-        elif mode == 'grad' or mode == 'func':
-            return self.smooth_multiplier * output
-        else:
-            raise ValueError("Mode incorrectly specified")
-
-    def obj(self, x):
-        return self.smooth_eval(x,mode='func') + self.obj_rough(x)
-
-    def proximal(self, x, g, L, prox_control=None):
-        """
-        Compute the proximal optimization
-
-        prox_control: If not None, then a dictionary of parameters for the prox procedure
-        """
-        z = x - g / L
-        if prox_control is None:
-            return self._prox(z, L)
-        else:
-            return self._prox(z, L, **prox_control)
 
