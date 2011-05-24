fixed some sign errors with the affine objective -- affinetutorial seems to work OK now

code/regreg/blocks.py
@@ -2,37 +2,70 @@ import numpy as np
 import seminorm, smooth, algorithms
 import pylab
 
-#XXX TODO use the constraint module here
-
 class Block(object):
 
     r"""
-    A Block can solve a signal approximator problem of the form
+    A Block solves a signal approximator problem for a given atom, the form
+    of which depends on whether the atom is in constraint mode
+    or Lagrange mode. An atom has an affine transform, i.e. a map
 
     .. math::
 
-       \text{minimize} \frac{1}{2} \|Y - D^Tu\|^2_2 \ \text{s.t.} \ h^*(u)
+       \beta \mapsto D\beta + \alpha
+
+    and a support function. The canonical example, the :math:`\ell_1` norm,
+    is the support function of the :math:`\ell_{\infty}` ball.
+
+    If it's in Lagrange mode, the Block solves the following
+    dual problem.
+
+    .. math::
+
+       \text{minimize} \frac{1}{2} \|Y - D^Tu\|^2_2 - \alpha^T u \ \text{s.t.} \ h^*(u)
        \leq \lambda
+
+    where :math:`h^*(u) \leq \lambda` is the dual constraint of the atom.
+    If the atom was the *l1norm*, then the constraint is an
+    :math:`\ell_{\infty}` or box constraint.
+
+    If the atom is in constraint mode, the Block solves the following
+    dual problem
+
+    .. math::
+
+       \text{minimize} \frac{1}{2} \|Y - D^Tu\|^2_2 - \alpha^T u +
+       \lambda h^*(u)
+
+    If the atom was the *l1norm*, then the norm above is
+    :math:`\ell_{\infty}` norm.
 
     """
 
-    def __init__(self, atom, initial=None):
-        self.atom = atom
-        Y = np.zeros(atom.p)
-        if initial is None:
-            initial = np.zeros(atom.m) 
-        if not atom.noneD:
-            self.loss = smooth.smooth_function(smooth.squaredloss(atom.D.T, Y))
-        else:
-            self.loss = smooth.smooth_function(smooth.signal_approximator(Y))
+    def __init__(self, atom, initial=None, response=None):
 
-        dual_atom = atom.dual_constraint
-        prox = dual_atom.primal_prox
-        nonsmooth = dual_atom.evaluate_constraint
+        self.affine_transform = atom.affine_transform
+        if response is None:
+            response = np.zeros(atom.primal_shape)
+        if initial is None:
+            initial = np.zeros(atom.dual_shape) 
+        else:
+            initial = initial.copy()
+        if atom.affine_transform.linear_operator is not None:
+            self.loss = smooth.affine_atom(smooth.l2normsq, atom.affine_transform.linear_operator.T, -response, l=0.5, diag=atom.affine_transform.diagD)
+        else:
+            self.loss = smooth.l2normsq.shift(-response, l=0.5)
+        if atom.affine_transform.affine_offset is not None:
+            affine_objective = smooth.linear(-atom.affine_transform.affine_offset, l=1)
+            self.objective = smooth.smooth_function(self.loss, affine_objective, l=1)
+        else:
+            self.objective = self.loss
+
+        prox = atom.dual_prox
+        nonsmooth = atom.evaluate_dual_constraint
         if nonsmooth(initial) == np.inf:
             raise ValueError('initial point is not feasible')
         
-        self.problem = seminorm.dummy_problem(self.loss.smooth_eval, nonsmooth, prox, initial)
+        self.problem = seminorm.dummy_problem(self.objective.smooth_eval, nonsmooth, prox, initial)
 
     def fit(self, *solver_args, **solver_kw):
         if not hasattr(self, '_solver'):
@@ -47,30 +80,32 @@ class Block(object):
         return self.problem.coefs
     coefs = property(get_coefs, set_coefs)
     
-
-    def set_Y(self, Y):
-        self.loss.atoms[0].Y[:] = Y
-    def get_Y(self):
-        return self.loss.atoms[0].Y
-    Y = property(get_Y, set_Y)
+    # response is the negative offset in the loss which is (l2normsq / 2.)
+    def set_response(self, response):
+        self.loss.affine_transform.affine_offset[:] = -response
+    def get_response(self):
+        return -self.loss.affine_transform.affine_offset.copy()
+    response = property(get_response, set_response)
     
-def dual_blocks(semi, Y, initial=None):
+def dual_blocks(atoms, current_resid, initial=None):
+    """
+    Optional initial dual variables.
+    """
     blocks = []
     if initial is None:
-        initial = [None] * len(semi.atoms)
-    for atom, atom_initial in zip(semi.atoms, initial):
-        blocks.append(Block(atom, initial=atom_initial))
+        initial = [None] * len(atoms)
+    for atom, atom_initial in zip(atoms, initial):
+        blocks.append(Block(atom, initial=atom_initial, response=current_resid))
     return blocks
 
-def blockwise(semi, Y, initial=None, max_its=50, tol=1.0e-06,
+def blockwise(atoms, response, initial=None, max_its=50, tol=1.0e-06,
               min_its=5):
 
-    blocks = dual_blocks(semi, Y)
-    current_resid = Y.copy() 
-    adjusted_resid = Y.copy()
-    primal_soln = current_resid
+    current_resid = response.copy() 
+    blocks = dual_blocks(atoms, current_resid, initial=initial)
+    primal_soln = current_resid.copy()
     for block in blocks:
-        current_resid -= block.atom.adjoint_map(block.coefs)
+        current_resid -= block.affine_transform.adjoint_map(block.coefs)
 
     for itercount in range(max_its):
         for block in blocks:
@@ -80,12 +115,12 @@ def blockwise(semi, Y, initial=None, max_its=50, tol=1.0e-06,
             # current resid that should be reset by a scatter
             # before each block updates its coefficients
 
-            block.Y = current_resid + block.atom.adjoint_map(block.coefs)
+            block.response = current_resid + block.affine_transform.adjoint_map(block.coefs)
             block.fit(max_its=800,tol=1e-10)
-            current_resid[:] = block.Y - block.atom.adjoint_map(block.coefs)
+            current_resid[:] = block.response - block.affine_transform.adjoint_map(block.coefs)
             if np.linalg.norm(primal_soln - current_resid) / np.max([1.,np.linalg.norm(current_resid)]) < tol and itercount >= min_its:
                 return current_resid
-            primal_soln = current_resid
+            primal_soln = current_resid.copy()
     return primal_soln
 
 def test1():
@@ -96,7 +131,7 @@ def test1():
     from regreg.algorithms import FISTA
     from regreg.atoms import l1norm
     from regreg.seminorm import seminorm, dummy_problem
-    from regreg.smooth import signal_approximator, smooth_function
+    from regreg.smooth import l2normsq
 
     Y = np.random.standard_normal(500); Y[100:150] += 7; Y[250:300] += 14
 
@@ -104,14 +139,13 @@ def test1():
     #Create D
     D = (np.identity(500) + np.diag([-1]*499,k=1))[:-1]
     D = sparse.csr_matrix(D)
-    fused = l1norm(D, l=19.5)
+    fused = l1norm.linear(D, l=19.5)
 
     pen = seminorm(sparsity,fused)
-    loss = smooth_function(signal_approximator(Y))
+    loss = l2normsq.shift(-Y, l=0.5)
     p = loss.add_seminorm(pen)
-
     
-    soln1 = blockwise(pen, Y)
+    soln1 = blockwise([sparsity, fused], Y)
 
     solver = FISTA(p)
     solver.fit(max_its=800,tol=1e-10)
@@ -134,13 +168,13 @@ def test2():
     from regreg.algorithms import FISTA
     from regreg.atoms import l1norm
     from regreg.seminorm import seminorm, dummy_problem
-    from regreg.smooth import signal_approximator, smooth_function
+    from regreg.smooth import signal_approximator
 
     n1, n2 = l1norm(1), l1norm(1)
     s=seminorm(n1,n2)
     Y = np.array([30.])
-    l=smooth_function(signal_approximator(Y))
-    blockwise(s, Y,l.add_seminorm(s))
+    loss = l2normsq.shift(-Y, l=0.5)
+    blockwise([n1, n2], Y)
 
 
 if __name__ == "__main__":

code/regreg/seminorm.py
@@ -156,13 +156,13 @@ class seminorm(object):
         affine_objective = 0
         if mode == 'func':
             for atom, segment in zip(self.atoms, self.dual_segments):
-                affine_objective += atom.affine_objective(v[segment])
+                affine_objective -= atom.affine_objective(v[segment])
             return (residual**2).sum() / 2. + affine_objective
         elif mode == 'both' or mode == 'grad':
             g = np.zeros((), self.dual_dtype)
             for atom, segment in zip(self.atoms, self.dual_segments):
                 g[segment] = -atom.affine_map(residual)
-                affine_objective += atom.affine_objective(v[segment])
+                affine_objective -= atom.affine_objective(v[segment])
             if mode == 'grad':
                 # XXX dtype manipulations -- would be nice not to have to do this
                 return g.reshape((1,)).view(np.float)