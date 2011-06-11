c9729489b98805d3d29f595fbb0156f96f493c7a
Jonathan Taylor <jonathan.taylor@stanford.edu>
2011-06-10 17:39:55 -0700

    small changes to examples

--- doc/examples/nearlyisotonic.py
+++ doc/examples/nearlyisotonic.py
@@ -4,7 +4,6 @@ from scipy import sparse
 
 import regreg.api as R
 
-
 n = 100
 Y = np.random.standard_normal(n)
 Y[:-30] += np.arange(n-30) * 0.2
@@ -12,19 +11,19 @@ Y[:-30] += np.arange(n-30) * 0.2
 D = (np.identity(n) - np.diag(np.ones(n-1),-1))[1:]
 nisotonic = R.positive_part.linear(-sparse.csr_matrix(D), lagrange=3)
 
-
 loss = R.l2normsq.shift(-Y,coef=0.5)
 p = R.container(loss, nisotonic)
-solver=R.FISTA(p.composite())
+solver = R.FISTA(p.composite())
 
 vals = solver.fit(max_its=25000, tol=1e-05)
 soln = solver.composite.coefs.copy()
 
-nisotonic.atoms[0].lagrange = 100.
+nisotonic.atom.lagrange = 1.
 solver.fit(max_its=25000, tol=1e-05)
 soln2 = solver.composite.coefs.copy()
 
-nisotonic.atoms[0].lagrange = 1000.
+nisotonic.atom.lagrange = 100.
+solver = R.FISTA(p.composite())
 solver.fit(max_its=25000, tol=1e-05)
 soln3 = solver.composite.coefs.copy()
 
