141dc4f7e469d21ad0248694bb419e34af96b2e2
Matthew Brett <matthew.brett@gmail.com>
2011-06-11 22:47:30 +0100

    NEW+TEST - affine transform test and generalize
    Generalize affine transforms to (N,), (N,1), (N,M) input

--- code/regreg/affine.py
+++ code/regreg/affine.py
@@ -1,9 +1,65 @@
+from operator import add, mul
 import numpy as np
 from scipy import sparse
+
+
+def broadcast_first(a, b, op):
+    """ apply binary operation `op`, broadcast `a` over axis 1 if necessary
+
+    Parameters
+    ----------
+    a : ndarray
+        If a is 1D shape (N,), convert to shape (N,1) before appling `op`.  This
+        has the effect of making broadcasting happen over axis 1 rather than the
+        default of axis 0.
+    b : ndarray
+        If a is 1D shape (P,), convert to shape (N,1) before appling `op`
+    op : callable
+        binary operation to apply to `a`, `b`
+
+    Returns
+    -------
+    res : object
+        shape equal to ``b.shape``
+    """
+    shape = b.shape
+    if a.ndim == 1:
+        a = a[:,None]
+    if b.ndim == 1:
+        b = b[:,None]
+    return op(a, b).reshape(shape)
+
+
+class AffineError(Exception):
+    pass
+
 
 class affine_transform(object):
     
     def __init__(self, linear_operator, affine_offset, diag=False):
+        """ Create affine transform
+
+        Parameters
+        ----------
+        linear_operator : None or ndarray or sparse array or affine_transform
+            Linear part of affine transform implemented as array or as
+            affine_transform.  None results in no linear component.
+        affine_offset : None or ndarray
+            offset component of affine.  Only one of `linear_operator` and
+            `affine_offset` can be None, because we need an input array to
+            define the shape of the transform.
+        diag : {False, True}, optional
+            If True, interpret 1D `linear_operator` as the main diagonal of the
+            a diagonal array, so that ``linear_operator =
+            np.diag(linear_operator)``
+        """
+        # noneD - linear_operator is None
+        # sparseD - linear_operator is sparse
+        # affineD - linear_operator is an affine_transform
+        # diagD - linear_operator is 1D representation of diagonal
+        if linear_operator is None and affine_offset is None:
+            raise AffineError('linear_operator and affine_offset cannot '
+                              'both be None')
         self.affine_offset = affine_offset
         self.linear_operator = linear_operator
 
@@ -28,7 +84,6 @@ class affine_transform(object):
                                  'affine_map',
                                  'affine_offset',
                                  'adjoint_map',
-                                 'affine_objective',
                                  'primal_shape',
                                  'dual_shape']]):
                 self.primal_shape = self.linear_operator.primal_shape
@@ -53,14 +108,28 @@ class affine_transform(object):
                 self.affineD = False
 
     def linear_map(self, x, copy=True):
-        r"""
+        r"""Apply linear part of transform to `x`
+
         Return :math:`Dx`
 
-        This routine is subclassed in affine_atom
-        as a matrix multiplications, but could
-        also call FFTs if D is a DFT matrix, in a subclass.
-        """
+        Parameters
+        ----------
+        x : ndarray
+            array to which to apply transform.  Can be 1D or 2D
+        copy : {True, False}, optional
+            If True, in situations where return is identical to `x`, ensure
+            returned value is a copy.
 
+        Returns
+        -------
+        Dx : ndarray
+            `x` transformed with linear component
+
+        Notes
+        -----
+        This routine is subclassed in affine_atom as a matrix multiplications,
+        but could also call FFTs if D is a DFT matrix, in a subclass.
+        """
         if self.noneD:
             # this sometimes has to be a copy
             # because the array can later be modified
@@ -70,67 +139,100 @@ class affine_transform(object):
             return x
         elif self.affineD:
             return self.linear_operator.linear_map(x)
-        else:
-            if self.sparseD or self.diagD:
-                return self.linear_operator * x
-            else:
-                return np.dot(self.linear_operator, x)
+        elif self.sparseD:
+            return self.linear_operator * x
+        elif self.diagD:
+            # Deal with 1D or 2D input or linear operator
+            return broadcast_first(self.linear_operator, x, mul)
+        return np.dot(self.linear_operator, x)
 
     def affine_map(self, x, copy=True):
-        r"""
+        r"""Apply linear and affine offset to `x`
+
         Return :math:`Dx+\alpha`
 
-        This routine is subclassed in affine_atom
-        as a matrix multiplications, but could
-        also call FFTs if D is a DFT matrix, in a subclass.
-        """
+        Parameters
+        ----------
+        x : ndarray
+            array to which to apply transform.  Can be 1D or 2D
+        copy : {True, False}, optional
+            If True, in situations where return is identical to `x`, ensure
+            returned value is a copy.
 
+        Returns
+        -------
+        Dx_a : ndarray
+            `x` transformed with linear and offset components
+
+        Notes
+        -----
+        This routine is subclassed in affine_atom as a matrix multiplications,
+        but could also call FFTs if D is a DFT matrix, in a subclass.
+        """
         if self.affineD:
             v = self.linear_operator.affine_map(x)
         else:
             v = self.linear_map(x, copy)
         if self.affine_offset is not None:
-            return self.affine_offset + v
-        else:
-            # if copy is True, v will already be a copy, so no need to check 
-            # again
-            return v
+            # Deal with 1D and 2D input, affine_offset cases
+            return broadcast_first(self.affine_offset, v, add)
+        # if copy is True, v will already be a copy, so no need to check again
+        return v
 
     def adjoint_map(self, u, copy=True):
-        r"""
+        r"""Apply transpose of linear component to `u`
+
         Return :math:`D^Tu`
 
+        Parameters
+        ----------
+        u : ndarray
+            array to which to apply transposed linear part of transform. Can be
+            1D or 2D array
+        copy : {True, False}, optional
+            If True, in situations where return is identical to `u`, ensure
+            returned value is a copy.
+
+        Returns
+        -------
+        DTu : ndarray
+            `u` transformed with transpose of linear component
+
+        Notes
+        -----
         This routine is currently a matrix multiplication, but could
         also call FFTs if D is a DFT matrix, in a subclass.
         """
-        if not self.noneD:
-            if self.sparseD or self.diagD:
-                if self.sparseD_csr:
-                    return self.linear_operator_T * u
-                else:
-                    return u * self.linear_operator
-            elif self.affineD:
-                return self.linear_operator.adjoint_map(u)
-            else:
-                return np.dot(u, self.linear_operator)
-        else:
-            # this might have to be a copy
-            # but we only multiply by D.T when
-            # computing gradient -- 
-            # this currently doesn't happen in seminorm or
+        if self.noneD:
+            # this might have to be a copy but we only multiply by D.T when
+            # computing gradient -- this currently doesn't happen in seminorm or
             # smoothed_seminorm
             if copy:
                 return u.copy()
-            else:
-                return u
+            return u
+        if self.sparseD_csr:
+            return self.linear_operator_T * u
+        if self.sparseD:
+            return self.linear_operator.T * u
+        if self.diagD:
+            # Deal with 1D or 2D input or linear operator
+            return broadcast_first(self.linear_operator, u, mul)
+        if self.affineD:
+            return self.linear_operator.adjoint_map(u)
+        return np.dot(self.linear_operator.T, u)
+
 
 class linear_transform(affine_transform):
-
+    """ A linear transform is an affine transform with no affine offset
+    """
     def __init__(self, linear_operator, diag=False):
+        if linear_operator is None:
+            raise AffineError('linear_operator cannot be None')
         affine_transform.__init__(self, linear_operator, None, diag=diag)
 
     def affine_map(self, u):
         raise ValueError('linear transforms have no affine part')
+
 
 class selector(object):
 
