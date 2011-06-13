120008a348be8b2f78b09d3da5c0f58d61f8ec4a
Jonathan Taylor <jonathan.taylor@stanford.edu>
2011-06-13 13:45:19 -0700

    moving plots in tutorials to examples directory so they can be run separately

---
+++ doc/examples/basispursuit_tutorial.py
@@ -0,0 +1,47 @@
+
+import numpy as np
+import scipy.linalg
+import pylab
+
+X = np.random.standard_normal((500,1000))
+
+beta = np.zeros(1000)
+beta[:100] = 3 * np.sqrt(2 * np.log(1000))
+
+Y = np.random.standard_normal((500,)) + np.dot(X, beta)
+Xnorm = scipy.linalg.eigvalsh(np.dot(X.T,X), eigvals=(998,999)).max()
+
+import regreg.api as R
+from regreg.smooth import linear
+smooth_linf_constraint = R.smoothed_atom(R.maxnorm(1000, bound=1),
+                                        epsilon=0.01,
+                                        store_argmin=True)
+loss = R.affine_smooth(smooth_linf_constraint, -X.T, None)
+smooth_f = R.smooth_function(loss, linear(Y))
+
+
+norm_Y = np.linalg.norm(Y)
+l2_constraint_value = np.sqrt(0.1) * norm_Y
+l2_lagrange = R.l2norm(500, lagrange=l2_constraint_value)
+
+basis_pursuit = R.container(smooth_f, l2_lagrange)
+solver = R.FISTA(basis_pursuit.composite(initial=np.random.standard_normal(500)))
+tol = 1.0e-08
+
+for epsilon in [0.6**i for i in range(20)]:
+   smooth_linf_constraint.epsilon = epsilon
+   solver.composite.lipshitz = 1.1/epsilon * Xnorm
+   solver.fit(max_its=2000, tol=tol, min_its=10, backtrack=False)
+
+basis_pursuit_soln = smooth_linf_constraint.argmin
+
+sparsity = R.l1norm(1000, bound=np.fabs(basis_pursuit_soln).sum())
+loss = R.l2normsq.affine(X, -Y)
+lasso = R.container(loss, sparsity)
+lasso_solver = R.FISTA(lasso.composite())
+lasso_solver.fit(max_its=2000, tol=1.0e-10)
+lasso_soln = lasso_solver.composite.coefs
+
+pylab.plot(basis_pursuit_soln, label='Basis pursuit')
+pylab.plot(lasso_soln, label='LASSO')
+pylab.legend()
--- doc/fusedtutorial.rst
+++ doc/fusedtutorial.rst
@@ -105,28 +105,5 @@ Here max_its represents primal (outer) iterations, and tol is the primal toleran
 
 We can then plot solution to see the result of the regression,
 
-.. plot::
-
-   import numpy as np
-   import pylab	
-   from scipy import sparse
-   from regreg.algorithms import FISTA
-   from regreg.atoms import l1norm
-   from regreg.container import container
-   from regreg.smooth import signal_approximator, smooth_function
-
-   Y = np.random.standard_normal(500); Y[100:150] += 7; Y[250:300] += 14
-   loss = smooth_function(signal_approximator(Y))
-   sparsity = l1norm(len(Y), lagrange=0.8)
-   sparsity.lagrange += 1
-   D = (np.identity(500) + np.diag([-1]*499,k=1))[:-1]
-   D = sparse.csr_matrix(D)
-   fused = l1norm.linear(D, lagrange=25.5)
-   problem = container(loss, sparsity, fused)
-   solver = FISTA(problem.composite())
-   solver.fit(max_its=100, tol=1e-10)
-   solution = solver.composite.coefs
-   pylab.plot(solution, c='g', linewidth=3)	
-   pylab.scatter(np.arange(Y.shape[0]), Y)
-
+.. plot:: ./examples/fusedtutorial.py
 
