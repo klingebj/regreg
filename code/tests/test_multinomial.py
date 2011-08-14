38d5e3fa6d43fba1bb3f30cadade5c89e1044c2c
klingebj <bjk@stanford.edu>
2011-08-14 14:16:04 -0700

    Adding a test for multinomial regression

---
+++ code/tests/test_multinomial.py
@@ -0,0 +1,35 @@
+import numpy as np
+from numpy import testing as npt
+from numpy.testing import *
+import regreg.api as rr
+
+@dec.setastest(True)
+def test_multinomial_vs_logistic():
+
+    """
+    Test that multinomial regresison with two categories is the same as logistic regression
+    """
+
+    n = 500
+    p = 10
+    J = 2
+
+    X = np.random.standard_normal(n*p).reshape((n,p))
+    counts = np.random.randint(0,10,n*J).reshape((n,J)) 
+
+    mult_x = rr.linear_transform(X, primal_shape=(p,J-1))
+    loss = rr.multinomial_loglikelihood.linear(mult_x, counts=counts)
+    problem = rr.container(loss)
+    solver = rr.FISTA(problem)
+    solver.fit(debug=False, tol=1e-10)
+    coefs1 = solver.composite.coefs
+
+    loss = rr.logistic_loglikelihood.linear(X, successes=counts[:,0], trials = np.sum(counts, axis=1))
+    problem = rr.container(loss)
+    solver = rr.FISTA(problem)
+    solver.fit(debug=False, tol=1e-10)
+    coefs2 = solver.composite.coefs
+
+    npt.assert_equal(coefs1.shape,(p,J-1))
+    npt.assert_array_almost_equal(coefs1.flatten(), coefs2.flatten(), 5)
+
