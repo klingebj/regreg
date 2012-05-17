changing sign of offset in identity_quadratic; fixing simple_problem

code/regreg/atoms.py
@@ -249,7 +249,7 @@ class atom(nonsmooth):
 
         """
 
-        proxq = identity_quadratic(lipschitz, -x, grad)
+        proxq = identity_quadratic(lipschitz, x, grad)
         totalq = self.quadratic + proxq
 
         if self.offset is None or np.all(np.equal(self.offset, 0)):
@@ -258,7 +258,7 @@ class atom(nonsmooth):
             offset = self.offset
 
         if offset is not None:
-            totalq.offset = -offset
+            totalq.offset += offset
             totalq = totalq.collapsed()
 
         if totalq.coef == 0:

code/regreg/cones.py
@@ -148,7 +148,7 @@ class cone(nonsmooth):
 
         """
 
-        proxq = identity_quadratic(lipschitz, -x, grad)
+        proxq = identity_quadratic(lipschitz, x, grad)
         totalq = self.quadratic + proxq
 
         if self.offset is None or np.all(np.equal(self.offset, 0)):
@@ -157,7 +157,7 @@ class cone(nonsmooth):
             offset = self.offset
 
         if offset is not None:
-            totalq.offset = -offset
+            totalq.offset += offset
             totalq = totalq.collapsed()
 
         if totalq.coef == 0:

code/regreg/smooth.py
@@ -30,9 +30,6 @@ class smooth_atom(smooth_composite):
         else:
             self.set_quadratic(0,0,0,0)
 
-
-        raise NotImplementedError
-
     def smooth_objective(self, x, mode='both', check_feasibility=False):
         raise NotImplementedError
     
@@ -187,6 +184,17 @@ def squaredloss(linear_operator, offset, coef=1):
 
 def signal_approximator(offset, coef=1):
     return quadratic.shift(-offset, coef)
+
+class zero(smooth_atom):
+
+    def smooth_objective(self, x, mode='both', check_feasibility=False):
+        if mode == 'both':
+            return 0., np.zeros(x.shape)
+        elif mode == 'func':
+            return 0.
+        elif mode == 'grad':
+            return np.zeros(x.shape)
+        raise ValueError("Mode not specified correctly")
 
 class logistic_deviance(smooth_atom):
 