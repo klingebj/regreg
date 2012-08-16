b4c944108ce054eba8d73951f989b889ae6d87dc
Jonathan Taylor <jonathan.taylor@stanford.edu>
2012-08-16 09:36:47 -0700

    documentation on NESTA, TFOCS

--- code/regreg/simple.py
+++ code/regreg/simple.py
@@ -110,48 +110,161 @@ def gengrad(simple_problem, L, tol=1.0e-8, max_its=1000, debug=False,
         coef = newcoef
     return coef
 
-def nesta(smooth_atom, proximal_atom, atom, epsilon=None,
+def nesta(smooth_atom, proximal_atom, conjugate_atom, epsilon=None,
           tol=1.e-06):
+    '''
+    Parameters
+    ==========
+
+    smooth_atom: smooth_composite
+        A smooth function, i.e. having a smooth_objective method.
+
+    proximal_atom: 
+        An atom with a proximal method.
+
+    conjugate_atom:
+        An atom that will be smoothed, by adding a quadratic to its
+        conjugate.
+
+    epsilon: np.array
+        A decreasing array of positive constants for Moreau-Yosida smoothing.
+        
+    tol: np.float
+        Tolerance to which each problem is solved is max(tol, epsilon)
+    
+    Returns
+    =======
+
+    primal: np.array
+        Primal coefficients.
+
+    dual: np.array
+        Dual coefficients.
+
+    '''
     if epsilon is None:
         epsilon = 2.**(-np.arange(20))
 
-    transform, conjugate = atom.dual
-    dual_coef = np.zeros(atom.dual_shape)
+    transform, conjugate = conjugate_atom.dual
+    dual_coef = np.zeros(conjugate_atom.dual_shape)
     for eps in epsilon:
-        smoothed = atom.smoothed(identity_quadratic(eps, dual_coef, 0, 0))
+        smoothed = conjugate_atom.smoothed(identity_quadratic(eps, dual_coef, 0, 0))
         if smooth_atom is not None:
             final_smooth = smooth_sum([smooth_atom, smoothed])
         else:
             final_smooth = smoothed
         problem = simple_problem(final_smooth, proximal_atom)
-        primal_coef = problem.solve(tol=tol)
+        primal_coef = problem.solve(tol=max(eps,tol))
         # when there's an affine transform involved
-        dual_coef = smoothed.grad
+        dual_coef[:] = smoothed.grad
     
     return primal_coef, dual_coef
 
-def tfocs(atom_presmooth, transform, proximal_atom, epsilon=None,
+def tfocs(primal_atom, transform, dual_proximal_atom, epsilon=None,
           tol=1.e-06):
+    '''
+
+    This function is based on the setup of problems
+    described in `TFOCS <http://tfocs.stanford.edu/>`_.
+    Generally speaking, these are the same type of problems
+    that nesta can handle, though without the additional smooth part.
+
+    This solver is suited to solving problems of the form
+
+    minimize_v f(v) + h(Dv+a)
+
+    when both f and h (and hence f^* and h^*) 
+    have simple proximal operators.
+
+    Here is an example for minimum :math:`\ell_1` norm reconstruction.
+
+    >>> import numpy as np, regreg.api as rr
+    >>> n, p = 200, 5000
+
+    The problem assumes Y=np.dot(X,beta) for some sparse beta.
+
+    >>> X = np.random.standard_normal((n, p))
+    >>> beta = np.zeros(p)
+    >>> beta[:10] = 10
+    >>> Y = np.dot(X, beta)
+    >>> 
+
+    The problem is formally,
+
+    minimize_v np.fabs(v).sum() subject to Y=np.dot(X,v)
+
+    The :math:`\ell_1` norm is described as:
+
+    >>> l1 = rr.l1norm(p, lagrange=1)
+
+    The constraint is specified as
+
+    >>> constraint = rr.zero_constraint.affine(X,-Y)
+
+    >>> transform, zero = constraint.dual
+    >>> primal_tfocs, dual_tfocs = rr.tfocs(l1, transform, zero)
+    >>> primal_tfocs[:20]
+    array([ 10.00000055,  10.00000043,  10.00000038,  10.00000073,
+             9.99999991,  10.00000058,   9.99999946,  10.00000004,
+             9.99999981,  10.00000028,   0.        ,  -0.        ,
+            -0.        ,   0.        ,   0.        ,  -0.        ,
+             0.        ,   0.        ,  -0.        ,   0.        ])
+    >>> np.linalg.norm(primal_tfocs[10:])
+    0.0
+    >>> 
+
+    Parameters
+    ==========
+
+    primal_atom: atom
+        An atom that will be smoothed,
+        then composed with the transform.
+
+    transform : affine_transform
+        An affine transform for the composition.
+
+    dual_proximal_atom: atom
+        An atom with a proximal method.
+
+    epsilon: np.array
+        A decreasing array of positive constants for Moreau-Yosida smoothing.
+        
+    tol: np.float
+        Tolerance to which each problem is solved is max(tol, epsilon)
+    
+    Returns
+    =======
+
+    primal: np.array
+        Primal coefficients.
+
+    dual: np.array
+        Dual coefficients.
+
+    
+    '''
     transform = astransform(transform)
-    #atom_presmooth needs a conjugate so it can be smoothed
-    atom_presmooth_c = atom_presmooth.conjugate
-    proximal_atom_c = proximal_atom.conjugate
+
+    #conjugate_atom needs a conjugate so it can be smoothed
+
+    conjugate_atom = primal_atom.conjugate
+
     if epsilon is None:
         epsilon = 2.**(-np.arange(20))
 
     offset = transform.affine_offset
     if offset is not None:
-        sq = identity_quadratic(0,0,-offset, 0)
+        dual_sq = identity_quadratic(0,0,-offset, 0)
     else:
-        sq = identity_quadratic(0,0,0,0)
+        dual_sq = identity_quadratic(0,0,0,0)
         
-    primal_coef = np.zeros(atom_presmooth.primal_shape)
+    primal_coef = np.zeros(conjugate_atom.primal_shape)
     for eps in epsilon:
-        smoothed = atom_presmooth_c.smoothed(identity_quadratic(eps, primal_coef, 0, 0))
+        smoothed = conjugate_atom.smoothed(identity_quadratic(eps, primal_coef, 0, 0))
         final_smooth = affine_smooth(smoothed, scalar_multiply(adjoint(transform), -1))
-        problem = simple_problem(final_smooth, proximal_atom_c)
-        dual_coef = problem.solve(sq, tol=tol)
+        problem = simple_problem(final_smooth, dual_proximal_atom)
+        dual_coef = problem.solve(dual_sq, tol=max(eps,tol))
         # when there's an affine transform involved
-        primal_coef = final_smooth.grad
+        primal_coef[:] = final_smooth.grad
     
     return primal_coef, dual_coef
